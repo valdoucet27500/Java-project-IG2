4ca35d164f93fd004911d5508e087ba13cff323b
Doucet Valentin <doucetvalentin27500@gmail.com>
2023-04-20 14:58:25 +0200

    Hello world in JavaFx

---
+++ BarProject/src/barPackage/Main.java
@@ -0,0 +1,26 @@
+package barPackage;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Main extends Application {
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/MainView.fxml")));
+        primaryStage.setTitle("Bar");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+
+    }
+}
